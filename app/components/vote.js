4a7b4c66ad7abd3ccfd0e300aef87527046fcfd2
Alexander Karle <akarle@umass.edu>
2016-03-07 16:54:49 -0500

    updated vote, voteThumbnail

--- app/components/vote.js
+++ app/components/vote.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import VoteThumbnail from './voteThumbnail';
+import VoteThumbnail from './voteThumbnail';
 //import StatusUpdateEntry from './statusupdateentry';
 //import {getFeedData, postStatusUpdate} from '../server';
 //import {getCandidate, getAllCandidates} from '../server';
@@ -11,7 +11,9 @@ export default class Vote extends React.Component {
     return (
       <div className="col-md-6 col-md-offset-3">
         <h1>Vote</h1>
+        <VoteThumbnail/>
       </div>
+
     );
   }
 }
