096ab11e135ff294fa7246823dee1e9b1b05f9eb
Alexander Karle <akarle@umass.edu>
2016-03-07 12:45:41 -0500

    updated parties in db

--- app/database.js
+++ app/database.js
@@ -69,6 +69,29 @@ var initialData = {
     }
   },
 
+  "parties": {
+    "1": {
+      "name": "Democratic Party",
+      "color": "#6194BC",
+      "logo": "img/partyLogos/democraticParty.png"
+    },
+    "2": {
+      "name": "Republican Party",
+      "color": "#FF4E4D",
+      "logo": "img/partyLogos/republicanParty.png"
+    },
+    "3": {
+      "name": "Green Party",
+      "color": "#805889",
+      "logo": "img/partyLogos/gp.png"
+    },
+    "4": {
+      "name": "Libertarian Party",
+      "color": "#805889",
+      "logo": "img/partyLogos/lib.png"
+    }
+  },
+
   "events": {
     "1": {
       "date": "",
