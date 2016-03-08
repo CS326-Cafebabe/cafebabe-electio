66b01f766e31ed175de96353194ad8c8f5da19ec
Elaina Bliss <ebliss@umass.edu>
2016-03-08 13:35:11 -0500

    Fixed colors on dynamic candidate pages

--- app/components/homeThumbnail.js
+++ app/components/homeThumbnail.js
@@ -29,6 +29,7 @@ export default class HomeThumbnail extends React.Component {
     var headImageText = (this.props.data.headImage);
     var modalID = ("cand-modal-" + this.props.uid);
     var modalIDTarget = ("#cand-modal-" + this.props.uid);
+    var cssModal = (this.props.data.thumbType + "-modal");
 
 
 
@@ -51,13 +52,13 @@ export default class HomeThumbnail extends React.Component {
                   </div>
 
                   <div className="row">
-                    <div className="col-md-4">
+                    <div className={"col-md-4 "+ cssModal}>
                         <img className="cand-pic" src={headImageText} width="100%" />
                         <hr/>
                         <a className="btn btn-default option-btn" href={this.props.data.campaignWebsite} role="button">Campaign Website</a>
                         <a className="btn btn-default option-btn" href={this.props.data.wikipedia} role="button">Wikipedia</a>
                     </div>
-                    <div className="col-md-8">
+                    <div className={"col-md-8 "+ cssModal}>
                       <h1 className="text-center cand-name text-uppercase">{this.props.data.fullName}</h1>
                       <p className="info">
                         <b className="quote"><i>{this.props.data.quote}</i></b>
@@ -70,10 +71,10 @@ export default class HomeThumbnail extends React.Component {
                     </div>
                   </div>
                   <div className="row">
-                    <div className="col-md-4">
+                    <div className={"col-md-4 "+ cssModal}>
                       <img src={this.props.data.logoImage} width="100%" />
                     </div>
-                    <div className="col-md-8 twitter">
+                    <div className={"col-md-8 "+ cssModal + " twitter"}>
                       <div className="panel panel-default twitter-panel">
                         <div className="panel-heading text-center">
                           <h4><b>What people are saying about {this.props.data.fullName}</b></h4>
