1b44df5a428f19cf33552425866dfa2edd53b8d6
Alexander Karle <akarle@umass.edu>
2016-03-09 00:54:24 -0500

    email settings loads dynamically, need to write to db still

--- app/components/candidateSub.js
+++ app/components/candidateSub.js
@@ -1,50 +1,70 @@
 import React from 'react';
+import {getParty} from '../server';
 
 export default class CandidateSub extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = props.on;
+    this.state = {
+      party : {
+        "name": "",
+        "color": "",
+        "logo": ""
+      }
+    };
   }
 
-  handleSub(clickEvent){
-      // Stop the event from propagating up the DOM tree, since we handle it here.
-      clickEvent.stopPropagation();
-      // 0 represents the 'main mouse button' -- typically a left click
-      if (clickEvent.button === 0) {
-        this.setState(1);
-        console.log(this.state);
-      }
+  // handleSub(clickEvent){
+  //     // Stop the event from propagating up the DOM tree, since we handle it here.
+  //     clickEvent.stopPropagation();
+  //     // 0 represents the 'main mouse button' -- typically a left click
+  //     if (clickEvent.button === 0) {
+  //       this.setState(1);
+  //       // console.log(this.state);
+  //     }
+  //
+  // }
+  //
+  // handleUnsub(clickEvent){
+  //     // Stop the event from propagating up the DOM tree, since we handle it here.
+  //     clickEvent.stopPropagation();
+  //     // 0 represents the 'main mouse button' -- typically a left click
+  //     if (clickEvent.button === 0) {
+  //       this.setState(0);
+  //       // console.log(this.state);
+  //     }
+  //
+  // }
 
-  }
+  refresh() {
+    getParty(this.props.data.party, (out) => {
+      this.setState({party: out});
+    })
+   }
 
-  handleUnsub(clickEvent){
-      // Stop the event from propagating up the DOM tree, since we handle it here.
-      clickEvent.stopPropagation();
-      // 0 represents the 'main mouse button' -- typically a left click
-      if (clickEvent.button === 0) {
-        this.setState(0);
-        console.log(this.state);
-      }
-
+  componentDidMount() {
+    this.refresh();
   }
 
   render() {
-    var cssActive = "active canSubActive";
-    console.log(this.state);
+
+    // var cssActive = "active canSubActive";
+    var cssColor = this.props.data.cssType + "-panel";
+    // console.log(this.state.party.name);
+    // var cssDark = this.props.data.cssType + "-inactive";
     return (
       <div>
-          <div className="panel panel-default dem-panel">
+          <div className={"panel panel-default " + cssColor}>
             <div className="panel-body">
               <div className="row">
                 <div className="col-md-7">
                   <div className="media">
                     <div className="media-left">
-                      <img className="candSubImg" src="img/candidateHeads/sandersHead.jpg" width="100%" />
+                      <img className="candSubImg" src={this.props.data.headImage} width="100%" />
                     </div>
                     <div className="media-body">
-                      <h4>Bernie Sanders</h4>
-                      Democratic Party
+                      <h4>{this.props.data.fullName}</h4>
+                      {this.state.party.name}
                     </div>
                   </div>
                 </div>
@@ -54,8 +74,8 @@ export default class CandidateSub extends React.Component {
                     <label className="btn btn-primary">
                       <input type="radio" name="options" id="onBernie" autoComplete="off"/> On
                     </label>
-                    <label className={"btn btn-primary" + cssActive}>
-                      <input type="radio" name="options" id="offBernie" autoComplete="off" onClick={(e)=> this.handleUnsub(e)}/> Off
+                    <label className="btn btn-primary active">
+                      <input type="radio" name="options" id="offBernie" autoComplete="off" defaultChecked/> Off
                     </label>
                   </div>
                 </div>
--- app/components/emailsettings.js
+++ app/components/emailsettings.js
@@ -1,24 +1,55 @@
 import React from 'react';
 import CandidateSub from './candidateSub';
-//import StatusUpdateEntry from './statusupdateentry';
-//import {getFeedData, postStatusUpdate} from '../server';
-//import {getCandidate, getAllCandidates} from '../server';
+import {getAllCandidates} from '../server';
 
 
 export default class EmailSettings extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      candidates: [
+        {
+          "headImage": "",
+          "logoImage": "",
+          "fullName": "",
+          "party": 1,
+          "thumbType": "",
+          "description": "",
+          "twitterFeed": "",
+          "campaignWebsite": "",
+          "wikipedia": "",
+          "age": "",
+          "quote": ""
+        }
+      ]
+    };
+  }
+
+  refresh() {
+    getAllCandidates((out) => {
+      this.setState({candidates: out});
+    })
+   }
+
+  componentDidMount() {
+    this.refresh();
+  }
 
   render() {
     return (
       <div>
 
         <div className="row">
-          <h1>Email Settings</h1> <hr/>
+          <h1>Email Settings</h1>
 
         </div>
 
         <div className="row">
           <div className="col-md-8 col-md-offset-2">
-            <CandidateSub on={0}/>
+            {this.state.candidates.map((candidate, i) =>
+              <CandidateSub key={i} uid={i} data={candidate}/>
+            )}
 
           </div>
         </div>
