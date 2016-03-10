fd957b63e1378bd698c121f59f3b6e91b97d470c
Alex Cobian <acobian@umass.edu>
2016-03-09 23:04:32 -0500

    Moved graph section from trends.js to trendGraph.js

--- app/components/trendGraph.js
+++ app/components/trendGraph.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Graph extends React.Component{
+export default class Graph extends React.Component {
   constructor(props) {
     super(props);
     this.state = props.data;
@@ -9,18 +9,39 @@ export default class Graph extends React.Component{
   render() {
     return (
       <div>
-        <hr />
         <div className="row">
           <div className="col-md-12">
-            <div className="panel panel-default upper-graph">
+            <div className="panel panel-default upper-graph-panel">
               <div className="panel-body">
                 <img className="img-responsive" src="img/graphs/graph1.png" id="upper-graph"/>
               </div>
             </div>
-            <p id="upper-summary"></p>
+            <p id="upper-summary">Overall summary of the {this.state.event.party} {this.state.event.name} based on user votes. {this.event.summary}</p>
           </div>
         </div>
-        <hr />
+        <hr/>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="panel panel-default mid-graph-panel">
+              <div className="panel-body">
+                <img className="img-responsive" src="img/graphs/graph1.png" id="mid-graph"/>
+              </div>
+            </div>
+            <p id="mid-summary"></p>
+          </div>
+        </div>
+        <hr/>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="panel panel-default lower-graph-panel">
+              <div className="panel-body">
+                <img className="img-responsive" src="img/graphs/graph1.png" id="lower-graph"/>
+              </div>
+            </div>
+            <p id="lower-summary">Literally why whon't this work</p>
+          </div>
+        </div>
+        <hr/>
       </div>
     );
   }
--- app/components/trends.js
+++ app/components/trends.js
@@ -1,91 +1,73 @@
 import React from 'react';
-import {getAllEvents} from '../server';
-//import {Graph} from './trendGraph';
+import { getAllEvents } from '../server';
+import { Graph } from './trendGraph';
 
-export default class Trends extends React.Component{
+export default class Trends extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       "events": []
     }
   }
 
-  handleClick(clickEvent, event){
+  handleClick(clickEvent, event) {
     clickEvent.preventDefault;
-    document.getElementById("upper-summary").innerHTML = "The overall summary for the "+event.party+" "+event.name+" based on users votes. "+event.summary;
-    document.getElementById("mid-summary").innerHTML = "The distribution of votes based on user gender information for the "+event.party+" "+event.name;
+    document.getElementById("react-graphs").event = event;
   }
+
   refresh() {
-    getAllEvents( (out) => {
+    getAllEvents((out) => {
       this.setState({events: out});
     })
-   }
+  }
 
   componentDidMount() {
     this.refresh();
   }
 
   render() {
-      return (
-        <div>
-          <div className="container-fluid">
-            <div className="col-md-12 content">
-              <div className="row">
-                <div className="col-md-12 trending">
-                  <h2>TRENDS <small>Graphical Information on Voting Patterns</small></h2>
-                  <hr />
-                </div>
+    return (
+      <div>
+        <div className="container-fluid">
+          <div className="col-md-12 content">
+            <div className="row">
+              <div className="col-md-12 trending">
+                <h2>TRENDS
+                  <small>Graphical Information on Voting Patterns</small>
+                </h2>
+                <hr/>
               </div>
+            </div>
 
-              <div className="row">
-                <div className="col-md-4 articles">
-                  <h3>Events</h3>
-                  <hr />
-                  <ul>
-                    {this.state.events.map((event, i) =>
-                      <li key={i}><button className="btn btn-default event-btn" onClick={(e)=>this.handleClick(e, event)}>{event.party} {event.name}</button></li>
-                    )}
-                    <li className="media">
-                      <div className="media-left media-top">
-                        <span className="caret"></span>
-                      </div>
-                      <div className="media-body">
-                        <a href="#/archive">More Past Events</a>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-                <div className="col-md-8 graphs">
-                  <h3>Activity</h3>
-                    <hr />
-                      <div className="row">
-                        <div className="col-md-12">
-                          <div className="panel panel-default upper-graph-panel">
-                            <div className="panel-body">
-                              <img className="img-responsive" src="img/graphs/graph1.png" id="upper-graph"/>
-                            </div>
-                          </div>
-                          <p id="upper-summary">Select an event</p>
-                        </div>
-                      </div>
-                    <hr />
-                      <div className="row">
-                        <div className="col-md-12">
-                          <div className="panel panel-default mid-graph-panel">
-                            <div className="panel-body">
-                              <img className="img-responsive" src="img/graphs/nhprimarygender.png" id="mid-graph"/>
-                            </div>
-                          </div>
-                          <p id="mid-summary"></p>
-                        </div>
-                      </div>
-                    <hr />
-                  </div>
-                </div>
+            <div className="row">
+              <div className="col-md-4 articles">
+                <h3>Events</h3>
+                <hr/>
+                <ul>
+                  {this.state.events.map((event, i) =>
+                    <li key={i}>
+                      <button className="btn btn-default event-btn" onClick={(e) => this.handleClick(e, event)}>{event.party} {event.name}</button>
+                    </li>)}
+                  <li className="media">
+                    <div className="media-left media-top">
+                      <span className="caret"></span>
+                    </div>
+                    <div className="media-body">
+                      <a href="#/archive">More Past Events</a>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-md-8 graphs">
+                <h3>Activity</h3>
+                <hr/>
+                Graphs go here!
               </div>
             </div>
           </div>
-      );
-    }
+        </div>
+      </div>
+    );
+  }
 }
