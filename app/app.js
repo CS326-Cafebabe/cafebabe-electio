81ca768799be8395e8e8b533aff87308155ef10b
Andrew McCluskey <andrew.m.mccluskey@googlemail.com>
2016-03-11 20:30:34 -0500

    Menu now updates the active page. React-router links are correct, closes #20, closes #23

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { IndexRoute, Router, Route, hashHistory } from 'react-router';
+import {IndexRoute, Router, Route, hashHistory} from 'react-router';
 import Home from './components/home';
 import Vote from './components/vote';
 import Archive from './components/archive';
@@ -8,12 +8,19 @@ import Trend from './components/trends';
 import ChatList from './components/chatList';
 import EmailSettings from './components/emailsettings';
 import Settings from './components/settings';
-import Sidebar from './components/sidebar';
+import {HomeSidebar,
+  TrendsSidebar,
+  ArchiveSidebar,
+  CalendarSidebar,
+  VoteSidebar,
+  ChatSidebar,
+  SettingsSidebar
+} from './components/sidebar';
 // import Calendar from './components/calendar';
 
 class VotePage extends React.Component {
   render() {
-      //<p>This is the vote page for party {this.props.params.party}.</p>
+    //<p>This is the vote page for party {this.props.params.party}.</p>
     return (
       <div>
         <Vote/>
@@ -23,66 +30,55 @@ class VotePage extends React.Component {
   }
 }
 
-
 class HomePage extends React.Component {
   render() {
 
     return (
-      //<p>This is the home page.</p>
-      <Home/>
-    );
+    //<p>This is the home page.</p> < Home />);
+    <Home />
+  )
   }
 }
 
-
-
 class SettingsPage extends React.Component {
   render() {
-    return (
-      <Settings userId={1}/>
-    );
+    return (<Settings userId={1}/>);
   }
 }
 
 class TrendPage extends React.Component {
   render() {
-    return (
-      <Trend />
-    );
+    return (<Trend/>);
   }
 }
 
 class ArchivePage extends React.Component {
   render() {
-    return (
-      <Archive page={this.props.params.page}/>
-    );
+    return (<Archive page={this.props.params.page}/>);
   }
 }
 
 class CalendarPage extends React.Component {
   render() {
     return (
-      <p> Check the Calendar page please</p>
+      <p>
+        Check the Calendar page please</p>
     );
   }
 }
 
 class ChatPage extends React.Component {
   render() {
-    return (
-      <ChatList />
-    );
+    return (<ChatList/>);
   }
 }
 
 class EmailSettingsPage extends React.Component {
   render() {
-    return (
-      <EmailSettings />
-    );
+    return (<EmailSettings/>);
   }
 }
+
 
 
 
@@ -106,20 +102,33 @@ ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
       {/* Show the Feed at / */}
-      <IndexRoute component={HomePage} />
-      <Route path="vote" component={VotePage} />
-      <Route path="calendar" component={CalendarPage} />
-      <Route path="archive" component={ArchivePage} />
-      <Route path="archive/:page" component={ArchivePage} />
-      <Route path="trends" component={TrendPage} />
-      <Route path="usersettings" component={SettingsPage} />
-      <Route path="chat" component={ChatPage} />
+      <IndexRoute component={HomePage}/>
+      <Route path="vote" component={VotePage}/>
+      <Route path="calendar" component={CalendarPage}/>
+      <Route path="archive" component={ArchivePage}/>
+      <Route path="archive/:page" component={ArchivePage}/>
+      <Route path="trends" component={TrendPage}/>
+      <Route path="usersettings" component={SettingsPage}/>
+      <Route path="chat" component={ChatPage}/>
       <Route path="emailsettings" component={EmailSettingsPage}/>
 
     </Route>
   </Router>
-),document.getElementById('renderPage'));
+), document.getElementById('renderPage'));
 
 ReactDOM.render((
-  <Sidebar userId={1}/>
-),document.getElementById('renderSidebar'));
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={HomeSidebar}/>
+      <Route path="vote" component={VoteSidebar}/>
+      <Route path="calendar" component={CalendarSidebar}/>
+      <Route path="archive" component={ArchiveSidebar}/>
+      <Route path="archive/:page" component={ArchiveSidebar}/>
+      <Route path="trends" component={TrendsSidebar}/>
+      <Route path="usersettings" component={SettingsSidebar}/>
+      <Route path="chat" component={ChatSidebar}/>
+      <Route path="emailsettings" component={SettingsSidebar}/>
+
+    </Route>
+  </Router>
+), document.getElementById('renderSidebar'));
--- app/components/sidebar.js
+++ app/components/sidebar.js
@@ -3,7 +3,54 @@ import {Link} from 'react-router';
 import {getUserData} from "../server";
 import {ResetDatabase} from "../database";
 
-export default class Sidebar extends React.Component {
+//HomeSidebar, TrendsSidebar, ArchiveSidebar, CalendarSidebar, VoteSidebar, ChatSidebar, SettingsSidebar
+
+//Actual use sidebars with active component highlighted
+export class HomeSidebar extends React.Component {
+  render() {
+    return (<Sidebar active="home" userId={1}/>);
+  }
+}
+
+export class TrendsSidebar extends React.Component {
+  render() {
+    return (<Sidebar active="trends" userId={1}/>);
+  }
+}
+
+export class ArchiveSidebar extends React.Component {
+  render() {
+    return (<Sidebar active="archive" userId={1}/>);
+  }
+}
+
+export class CalendarSidebar extends React.Component {
+  render() {
+    return (<Sidebar active="calendar" userId={1}/>);
+  }
+}
+
+export class VoteSidebar extends React.Component {
+  render() {
+    return (<Sidebar active="vote" userId={1}/>);
+  }
+}
+
+export class ChatSidebar extends React.Component {
+  render() {
+    return (<Sidebar active="chat" userId={1}/>);
+  }
+}
+
+export class SettingsSidebar extends React.Component {
+  render() {
+    return (<Sidebar active="settings" userId={1}/>);
+  }
+}
+
+
+//Standard sidebar object
+class Sidebar extends React.Component {
 
   constructor(props) {
     super(props);
@@ -20,6 +67,13 @@ export default class Sidebar extends React.Component {
     this.refresh();
   }
 
+  isActivePage(page){
+    if (page === this.props.active) {
+      return "active"
+    }
+    return ""
+  }
+
   render() {
     return(
 
@@ -28,27 +82,19 @@ export default class Sidebar extends React.Component {
 
         <h2>Menu</h2>
         <ul className="nav nav-sidebar">
-          <li className="active">
-            <a href="#/">Home <span className="glyphicon glyphicon-home sidebar-glyph"></span></a>
-          </li>
 
-          <li>
-            <a href="#/trends">Trends <span className="glyphicon glyphicon-stats sidebar-glyph"></span></a>
-          </li>
+          <li key="home" className={this.isActivePage("home")}> <Link to={"/"}>Home<span className="glyphicon glyphicon-home sidebar-glyph"/></Link> </li>
 
-          <li>
-            <a href="#/archive">Archive <span className="glyphicon glyphicon-folder-open sidebar-glyph"></span></a>
-          </li>
+          <li key="trends" className={this.isActivePage("trends")}> <Link to="/trends">Trends<span className="glyphicon glyphicon-stats sidebar-glyph"/></Link> </li>
 
-          <li>
-            <a href="#/calendar">Calendar <span className="glyphicon glyphicon-calendar sidebar-glyph"></span></a>
-          </li>
+          <li key="archive" className={this.isActivePage("archive")}> <Link to="/archive">Archive <span className="glyphicon glyphicon-folder-open sidebar-glyph"/></Link> </li>
 
-          <li>
-            <a href="#/vote">Vote <span className="glyphicon glyphicon-pencil sidebar-glyph"></span></a>
-          </li>
+          <li key="calendar" className={this.isActivePage("calendar")}> <Link to="/calendar">Calendar<span className="glyphicon glyphicon-calendar sidebar-glyph"/></Link> </li>
 
-        </ul>
+          <li key="vote" className={this.isActivePage("vote")}> <Link to="vote">Vote<span className="glyphicon glyphicon-pencil sidebar-glyph"/></Link> </li>
+
+
+    </ul>
 
         <hr/>
 
@@ -58,17 +104,14 @@ export default class Sidebar extends React.Component {
         }
         <div id="sidebar-username">{this.state.fullName.toUpperCase()}</div>
 
-          <li >
-            <a href="#/chat">Chat <span className="glyphicon glyphicon-comment sidebar-glyph"></span></a>
-          </li>
+          <li key="chat" className={this.isActivePage("chat")}> <Link to="/chat">Chat<span className="glyphicon glyphicon-comment sidebar-glyph"/></Link> </li>
 
-          <li>
-            <a href="#/usersettings">Settings <span className="glyphicon glyphicon-cog sidebar-glyph"></span></a>
-          </li>
+          <li key="usersettings" className={this.isActivePage("settings")}> <Link to="/usersettings">Settings<span className="glyphicon glyphicon-cog sidebar-glyph"/></Link> </li>
 
           <li>
             <a href="#logout">Logout <span className="glyphicon glyphicon-lock sidebar-glyph"></span></a>
           </li>
+
         </ul>
 
         <hr/>
